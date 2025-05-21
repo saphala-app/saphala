4c49cca831aa08cf7f3189c90f6798b4c3ebf12d
hproooo55 <roshni25dec@gmail.com>
2025-05-21 23:00:58 +0530

    resolved error with tailwind and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/common/mode-toggle";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-between">
       <h1>Saphala App</h1>
+      <ModeToggle/>
     </div>
   );
 }
